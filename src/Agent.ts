0c3b2322f80d94ac2bea2719268cfb32c9251ec5
eddiearc <eddievim@foxmail.com>
2025-04-08 23:45:24 +0800

    function call in a standardized way

--- src/Agent.ts
+++ src/Agent.ts
@@ -1,40 +1,45 @@
+import OpenAI from "openai";
 import ChatOpenAI from "./ChatOpenAI";
 import { Tool } from "./type";
 import zodToJsonSchema from "zod-to-json-schema";
 
 export default class Agent {
     private tools: Tool[];
-    private llm: ChatOpenAI;
 
     constructor(tools: Tool[]) {
         this.tools = tools;
-        this.llm = new ChatOpenAI(this.getToolsDefinition(tools))
     }
 
     async query(query: string) {
+        const llm = new ChatOpenAI(this.getToolsDefinition(this.tools))
+        let dialogues: OpenAI.Chat.ChatCompletionMessageParam[] = [{role: 'user', content: query}]
         while (true) {
-            const result = await this.llm.chat(query)
+            const result = await llm.chat(dialogues)
             console.log(result.message.content)
             // 对话结束
             if (result.finish_reason === 'stop') {
                 return
             }
             // 需要调用工具
-            let observation = ''
             if (result.finish_reason === 'tool_calls') {
                 const toolCall = result.message.tool_calls
                 if (!toolCall) return
-                // 依次调用工具
-                for (const tool of toolCall) {
+                // 并发调用工具
+                const asyncToolCalls = toolCall.map(async tool => {
                     const toolName = tool.function.name
                     const toolArgs = JSON.parse(tool.function.arguments)
                     console.log(`${toolName} -> ${tool.function.arguments}`)
                     const toolResult = await this.tools.find(t => t.name === toolName)?.execute(toolArgs) // 工具调用
                     console.log('Observation: ', toolResult) // 工具执行结果
-                    observation += `Observation: ${toolResult}`
-                }
+                    // URL_ADDRESS.openai.com/docs/guides/function-calling?api-mode=responses
+                    return {
+                        role: 'tool',
+                        tool_call_id: tool.id,
+                        content: toolResult || ''
+                    } as OpenAI.Chat.ChatCompletionMessageParam
+                })
+                dialogues = await Promise.all(asyncToolCalls)
             }
-            query = observation // 构建observation
             console.log('--------------------------------')
         }
     }
